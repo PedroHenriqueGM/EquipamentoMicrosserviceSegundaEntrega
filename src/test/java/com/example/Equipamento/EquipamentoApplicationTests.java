0c9fc6c48b71b77ece56cf43c94dec63fff30102
Pedro Magalhaes <phmagalhaes013@gmail.com>
2025-11-27 01:45:08 -0300

    Corrigindo code smells maiores novamente

--- src/test/java/com/example/Equipamento/EquipamentoApplicationTests.java
+++ src/test/java/com/example/Equipamento/EquipamentoApplicationTests.java
@@ -8,6 +8,8 @@ class EquipamentoApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// This test is intentionally empty.
+		// Its purpose is only to verify that the Spring application context loads correctly.
 	}
 
 }
