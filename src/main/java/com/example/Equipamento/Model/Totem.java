3e2a4633231972762620f8735f2ffb5af1740c11
Pedro Magalhaes <phmagalhaes013@gmail.com>
2025-11-27 00:35:19 -0300

    Consertando erro no setId do Totem

--- src/main/java/com/example/Equipamento/Model/Totem.java
+++ src/main/java/com/example/Equipamento/Model/Totem.java
@@ -29,7 +29,7 @@ public class Totem {
     public List<Tranca> getTrancas() { return trancas; }
     public void setTrancas(List<Tranca> trancas) { this.trancas = trancas; }
 
-    public void setId(long l) {
+    public void setId(long id) {
         this.id = id;
     }
 }
